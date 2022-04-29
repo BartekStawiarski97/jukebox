f5b53ca6b374ea8d771c7762bbb79c8aa978a237
Bartek <99047458@mydavinci.nl>
2022-04-29 13:05:30 +0200

    Functies verplaatst van QueueController naar Playlist Class

--- app/Classes/Playlist.php
+++ app/Classes/Playlist.php
@@ -23,4 +23,47 @@
 				session()->forget('songqueue.'.$index);
 			}
 		}
+
+		public function clearQueue(){
+			session()->forget('songqueue');
+		}
+
+		public function convertTime(){
+			//variables
+			$minutes = 0;
+			$seconds = 0;
+			$extraMinutes = 0;
+	
+			if(session()->has('songqueue')){
+				$songQueue = session('songqueue');
+			}else{
+				$songQueue = session();
+			}
+	
+			//foreach song in the queue
+			foreach($songQueue as $song){
+				//Returns associative array with detailed info about given date/time
+				$convertedTime = date_parse($song->duration);
+				$minutes += $convertedTime['minute'];
+				$seconds += $convertedTime['second'];
+				//int div checks how many times it fits in the first given number
+				$extraMinutes = intdiv($seconds, 60);
+				$minutes += $extraMinutes;
+				//returns the remaining seconds
+				//100 : 60 = 40 because 60 can only fit in once in 100
+				$seconds = $seconds % 60;
+				
+			}
+			$time = [ 'minute' => $minutes,'second' => $seconds];
+			return $time;
+		}
+
+		public function queueSession(){
+			if(session()->has('songqueue')){
+				$songQueue = session('songqueue');
+			}else{
+				$songQueue = session();
+			}
+
+		}
 	}
--- app/Http/Controllers/QueueController.php
+++ app/Http/Controllers/QueueController.php
@@ -19,7 +19,8 @@ class QueueController extends Controller
     }
 
     public function clearQueue(){
-        session()->forget('songqueue');
+        $playlist = new Playlist();
+        $playlist->clearQueue();
         return redirect('/'); 
     }
 
@@ -31,39 +32,12 @@ class QueueController extends Controller
     }
 
     public function queue(){
+        $playlist = new Playlist();
         return view('queue', [
             'queue' => session('songqueue'),
-            'queueTime' => $this->convertTime()
+            'queueTime' => $playlist->convertTime()
         ]);
     }
 
-    public function convertTime(){
-        //variables
-        $minutes = 0;
-        $seconds = 0;
-        $extraMinutes = 0;
-
-        if(session()->has('songqueue')){
-            $songQueue = session('songqueue');
-        }else{
-            $songQueue = session();
-        }
-
-        //foreach song in the queue
-        foreach($songQueue as $song){
-            //Returns associative array with detailed info about given date/time
-            $convertedTime = date_parse($song->duration);
-            $minutes += $convertedTime['minute'];
-            $seconds += $convertedTime['second'];
-            //int div checks how many times it fits in the first given number
-            $extraMinutes = intdiv($seconds, 60);
-            $minutes += $extraMinutes;
-            //returns the remaining seconds
-            //100 : 60 = 40 because 60 can only fit in once in 100
-            $seconds = $seconds % 60;
-            
-        }
-        $time = [ 'minute' => $minutes,'second' => $seconds];
-        return $time;
-    }
+    
 }
